6d5f104e898841f471619368edf48f82c4f46f97
rukpong.r@arcadiaapm.com <rukpong.r@arcadiaapm.com>
2021-12-06 02:52:50 +0700

    add list on page

--- greeter.js
+++ greeter.js
@@ -70,7 +70,7 @@ function testGetFlight() {
         return __generator(this, function (_a) {
             switch (_a.label) {
                 case 0:
-                    api = 'https://110.238.116.225/tmpflightinformation-qa/api/Airport/DropdownMenuArrivalAirport';
+                    api = 'http://110.238.116.225/tmpflightinformation-qa/api/Airport/DropdownMenuArrivalAirport';
                     return [4 /*yield*/, fetch(api)];
                 case 1:
                     response = _a.sent();
@@ -86,10 +86,56 @@ function testGetFlight() {
 //     .then(resp => {
 //         document.body.textContent = resp.map(u => u.activityId).toString();
 //     })
-testGetFlight().then(function (data) {
-    console.log(data.data);
-    var arr_data = data.data;
-    arr_data.forEach(function (value) {
-        console.log(value.employee_name);
+// testGetFlight().then((data) => {
+//     console.log(data.data);
+//     let arr_data = data.data;
+//     arr_data.forEach(function (value) {
+//         console.log(value.employee_name);
+//     });
+// });
+function getLoginGame() {
+    return __awaiter(this, void 0, void 0, function () {
+        var api, response, data;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    api = 'http://127.0.0.1:8000/';
+                    return [4 /*yield*/, fetch(api)];
+                case 1:
+                    response = _a.sent();
+                    return [4 /*yield*/, response.json()];
+                case 2:
+                    data = _a.sent();
+                    return [2 /*return*/, data];
+            }
+        });
     });
+}
+getLoginGame().then(function (data) {
+    console.log(data);
+    var ul = document.getElementById("thelist");
+    for (var game in data) {
+        console.log(game, data[game]);
+        var listItem = document.createElement("li");
+        listItem.innerHTML = game;
+        ul.appendChild(listItem);
+    }
+    // for (var i = 0; i < )
 });
+var counter = 4;
+// addelement();
+function addelement() {
+    // var completelist = document.getElementById("thelist");
+    // completelist.innerHTML += "<li>Item " + counter + "</li>";
+    // counter++;
+    var ul = document.getElementById("thelist");
+    var toppings = ["Tomato", "Cheese", "Pepperoni",
+        "Olives", "Jalapenos", "Pineapple", "Ham"];
+    for (var i = 0; i < toppings.length; i++) {
+        var topping = toppings[i];
+        // console.log(topping);
+        var listItem = document.createElement("li");
+        listItem.innerHTML = topping;
+        ul.appendChild(listItem);
+    }
+}
--- greeter.ts
+++ greeter.ts
@@ -47,10 +47,56 @@ async function testGetFlight() {
 //         document.body.textContent = resp.map(u => u.activityId).toString();
 //     })
 
-testGetFlight().then((data) => {
-    console.log(data.data);
-    let arr_data = data.data;
-    arr_data.forEach(function (value) {
-        console.log(value.employee_name);
-    });
-});
+// testGetFlight().then((data) => {
+//     console.log(data.data);
+//     let arr_data = data.data;
+//     arr_data.forEach(function (value) {
+//         console.log(value.employee_name);
+//     });
+// });
+
+async function getLoginGame() {
+    const api = 'http://127.0.0.1:8000/';
+    let response = await fetch(api);
+    let data = await response.json();
+    return data;
+}
+
+getLoginGame().then((data) => {
+    console.log(data);
+    var ul = document.getElementById("thelist");
+
+    for (var game in data) {
+        console.log(game, data[game]);
+
+        var listItem = document.createElement("li");
+        listItem.innerHTML = game;
+        
+        ul.appendChild(listItem);
+    }
+    
+    // for (var i = 0; i < )
+})
+
+var counter = 4;
+// addelement();
+
+function addelement() {
+    // var completelist = document.getElementById("thelist");
+    // completelist.innerHTML += "<li>Item " + counter + "</li>";
+    // counter++;
+    var ul = document.getElementById("thelist")
+
+    var toppings = ["Tomato", "Cheese", "Pepperoni", 
+                "Olives", "Jalapenos", "Pineapple", "Ham"];
+
+    for (var i = 0; i < toppings.length; i++) {
+        var topping = toppings[i];
+        // console.log(topping);
+        
+        var listItem = document.createElement("li");
+        listItem.innerHTML = topping;
+        
+        ul.appendChild(listItem);
+    }
+}
